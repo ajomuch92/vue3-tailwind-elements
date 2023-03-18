2f5c805ef66d6bc0e6c20fd8768477ea128e36ba
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-03-18 17:37:38 -0600

    feat: 🎸 Migrating teAlert component

---
+++ src/components/teAlert.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="alert" :class="getClass">
+    <slot name="default">
+      {{text}}
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'normal',
+    validator: (value: string) => ['normal','primary', 'success', 'info', 'warning', 'danger'].includes(value)
+  },
+  solid: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const getClass = computed(() => ({
+  [props.type]: true,
+  'solid': props.solid,
+}));
+</script>
+
+<style scoped>
+
+</style>
