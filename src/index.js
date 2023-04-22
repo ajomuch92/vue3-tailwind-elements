a5f5dd1d77f165f7b71ea1d011f1987e90f08c9a
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-22 16:26:44 -0600

    refactor: 💡 Updating exporting to install components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import HelloWorld from './components/HelloWorld.vue';
+import TeButton from './components/TeButton.vue';
 
-export {
-  HelloWorld,
-};
+export default function install(vueApp) {
+  vueApp.component('hello-world', HelloWorld);
+  vueApp.component('te-button', TeButton);
+}
