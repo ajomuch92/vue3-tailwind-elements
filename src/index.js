6ac5b69ae78c73d33d963b3ddd618f09b4d644b9
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-22 16:58:14 -0600

    feat: 🎸 Updating wayt to export plugin for tailwind

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import HelloWorld from './components/HelloWorld.vue';
 import TeButton from './components/TeButton.vue';
 
-import Plugin from './plugin';
 
 export default function install(vueApp) {
   vueApp.component('hello-world', HelloWorld);
   vueApp.component('te-button', TeButton);
 }
-
-export { Plugin };
