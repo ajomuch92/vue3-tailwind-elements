4018f6c8e88d156e78443b62a5f13fc729adf79f
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-22 17:23:22 -0600

    refactor: 💡 Updating example project

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
@@ -11,3 +11,4 @@ module.exports = {
     require('vue3-tailwind-elements/dist/plugin'),
   ],
 }
+
