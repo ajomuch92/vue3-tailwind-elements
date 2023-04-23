da943484fa27abb6555ceeb93f4ec62d2dde6505
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-23 03:41:52 -0600

    feat: 🎸 Adding new components to install method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import HelloWorld from './components/HelloWorld.vue';
+import TeAccordion from './components/teAccordion.vue';
 import TeButton from './components/TeButton.vue';
+import TeSpinner from './components/teSpinner.vue';
 
 
 export default function install(vueApp) {
-  vueApp.component('hello-world', HelloWorld);
+  vueApp.component('te-accordion', TeAccordion);
   vueApp.component('te-button', TeButton);
+  vueApp.component('te-spinner', TeSpinner);
 }
