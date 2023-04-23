eb998fb98d982f5b0619d11c775b0189f19c091e
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-23 03:17:31 -0600

    refactor: 💡 Updating example

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './node_modules/vue3-tailwind-elements/**/*.{js,ts,vue}'
   ],
   theme: {
     extend: {},
